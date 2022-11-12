09b0959bee9a6728bb9b6f0bf47d77837f77d7a9
Anastssiia <metelkinaanastasiia@gmail.com>
2022-11-12 17:24:38 +0300

    full project

---
+++ src/test/java/GettingOrderListTest.java
@@ -0,0 +1,19 @@
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.ValidatableResponse;
+import jdk.jfr.Description;
+import org.junit.Test;
+import scooter.Order.OrderClient;
+
+import static org.hamcrest.CoreMatchers.*;
+
+public class GettingOrderListTest {
+
+    @Test
+    @DisplayName("Success getting a list of orders")
+    @Description("Check getting a list of orders is not empty")
+    public void getOrderListTest() {
+        OrderClient orderClient = new OrderClient();
+        ValidatableResponse orderListResponse = orderClient.getOrdersList().statusCode(200);
+        orderListResponse.assertThat().body("orders.id", notNullValue());
+    }
+}
